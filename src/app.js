a84a925804a74e5ed8f961aa42f0398839ee4cd2
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-04-15 22:15:26 +0530

    add welcome route for API webservice

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+app.get("/welcome", (req, res) => {
+  res.send(
+    "<h1>Welcome to URL-Shortener API</h1><br><h2>Visit <a href='https://anuj-thakur-513.github.io/URL-Shortener/' target='_blank'>https://anuj-thakur-513.github.io/URL-Shortener/</a> to get more info on API calls available</h2>"
+  );
+});
 // import ROUTERS
 import v1 from "./routes/version1.routes.js";
 app.use("/v1", v1);
